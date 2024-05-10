e7a22a8799877d08b2738185742999fb1aacde5c
delikely <delikely@gmail.com>
2024-05-11 00:02:12 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,4 @@
-echo "new ZDI, commit"
 git config --global user.email delikely@gmail.com
 git config --global user.name delikely
 git add advisories
-git commit -m "update(`date +'%Y-%m-%d'`)"
+git commit --allow-empty -m "update(`date +'%Y-%m-%d'`)"
