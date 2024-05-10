450b5ed03dd7eedae4eb0d1613f867bf7eb73c59
delikely <delikely@gmail.com>
2024-05-10 23:52:15 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,10 +1,5 @@
-git diff --exit-code
-if [ "$?" !=  "0" ];then
-    echo "new ZDI, commit"
-    git config --global user.email delikely@gmail.com
-    git config --global user.name delikely
-    git add advisories
-    git commit -m "update(`date +'%Y-%m-%d'`)"
-fi
-
-sleep 5
+echo "new ZDI, commit"
+git config --global user.email delikely@gmail.com
+git config --global user.name delikely
+git add advisories
+git commit -m "update(`date +'%Y-%m-%d'`)"
